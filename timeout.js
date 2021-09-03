2421002a30009f54d0f0dd82262fb5e8c260a0c2
JunayedAhmed <junayedahmed11099@gmail.com>
2021-09-04 04:09:14 +0600

    setTimeout used

---
+++ timeout.js
@@ -0,0 +1,12 @@
+function doSomething(){
+    console.log('I am coding javascript')
+}
+console.log('first ami sobar age');
+console.log('second i am the second person');
+// setTimeout(doSomething,5000);
+setTimeout(function(){
+    console.log(`I'm using vs code`)
+},3000);
+setTimeout(()=>console.log('I am using arrow function'),2000);
+console.log('third ami 3 number bacca');
+console.log('fourth ami ek hali purno korsi')
